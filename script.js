387ab36d98f711f770d16fd350c16d1e4beabda7
caamomilaa <c.velazquezdamico@gmail.com>
2024-04-03 09:14:42 +0200

    update-casa

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const isAdult = (name,age) =>{
         console.log ('Hola ' + name + ' eres mayor de edad');
     }
     else{
-        console.log ('Hola' + name + ' eres menor de edad');
+        console.log ('Hola ' + name + ' eres menor de edad');
     }
 }
 isAdultResult = isAdult ('oscar', 13);
@@ -17,4 +17,55 @@ const number = (numberA, numberB) =>{
         console.log (numberB)
     }
 }
-numberResult = number (2,7)
+numberResult = number (2,7);
+
+const typeNumber = (numberC) =>{
+    if (numberC>0){
+        console.log ('postivo')
+    }
+    else if (numberC<0){
+        console.log ('negativo')
+    }
+    else if (numberC===0){
+        console.log ('cero')
+    }
+}
+typeNumberResult = typeNumber (5);
+
+//puedo poner numberA o tiene que ser numberC?
+
+const grades = (numberD, numberE, numberF) =>{
+    const average=(numberD+numberE+numberF) / 3;
+
+    if (average<5){
+        console.log ('Suspenso')
+    }
+    else if (average>=5){
+        console.log ('Aprobado')
+    }
+    else if (average>=8){
+        console.log ('Matrícula de Honor')
+    }
+}
+gradesResult = grades (9, 9, 9);
+
+//por que no me sale MH?
+
+const biggerNumber = (numberG, numberH, numberI) =>{
+    if (numberG>numberH && numberI){
+        console.log (numberG)
+    }
+    else{
+        console.log (numberH && numberI)
+    }
+
+}
+biggerNumberResult = biggerNumber (5,7,2);
+
+//    if (numberG>numberI){
+// console.log (numberG)
+  //  }
+    //else{
+   // console.log (numberI)
+//} 
+
